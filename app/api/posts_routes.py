ffb0c5d8087eea2e4e7cf5a38de73dbd9c48a850
Jake Payne <paynekid96@gmail.com>
2021-12-07 11:05:12 -0700

    post for post form written

--- app/api/posts_routes.py
+++ app/api/posts_routes.py
@@ -1,11 +1,25 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from flask_login import login_required
 from app.models import Post, Photo
 from wtforms.validators import DataRequired
+from forms.post_form import PostForm
+from datetime import datetime
+from app.models import db
 
 
 
 posts_routes = Blueprint('posts', __name__)
+
+def validation_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f'{field} : {error}')
+    return errorMessages
+
 
 @posts_routes.route('/all')
 @login_required
@@ -24,6 +38,29 @@ def posts():
     # posts = db.Post.query.join(db.Like).join(db.Photo).filter(db.Post.user_id)
     # return dict.fromkeys(posts, "#")
 
-@posts_routes.route('/', methods=["POST"])
+@posts_routes.route('/new', methods=["POST"])
 @login_required
 def create_post():
+    form=PostForm()
+
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        post = Post(
+            user_id=form.data['user_id'],
+            description=form.data['description'],
+            createdAt= datetime.datetime.now,
+            updatedAt= datetime.datetime.now
+        )
+        db.session.add(post)
+        db.session.commit()
+        photo = Photo(
+            url=form.data['url'],
+            user_id=form.data['user_id'],
+            post_id=post.id,
+            createdAt= datetime.datetime.now,
+            updatedAt= datetime.datetime.now
+        )
+        db.session.add(photo)
+        db.session.commit()
+        return post.to_dict()
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 400
