73eb5569a0d177dbe88c0b295fed0950e553a9db
hauck29 <anthony.hauck@aol.com>
2021-12-07 08:20:08 -0800

    file cleanup, extra comments deleted

--- app/models/follows.py
+++ app/models/follows.py
@@ -7,14 +7,3 @@ follows = db.Table(
     db.Column('followed_id', db.Integer, db.ForeignKey('users.id'))
 )
 
-# class Follow(db.Model):
-#     __tablename__ = 'follows'
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     other_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-#     createdAt = db.Column(db.DateTime, nullable=False)
-#     updatedAt = db.Column(db.DateTime, nullable=False)
-
-#     user = db.relationship('User', back_populates='follows')
-#     other_user = db.relationship('User', back_populates='follows')
