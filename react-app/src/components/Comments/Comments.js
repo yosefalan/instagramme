e913f4aa98b297f20c0932c528b829fc5c759ead
hauck29 <anthony.hauck@aol.com>
2021-12-09 11:21:42 -0800

    comments routes complete

--- react-app/src/components/Comments/Comments.js
+++ react-app/src/components/Comments/Comments.js
@@ -1,32 +1,33 @@
-import React from 'react'
-import {getAllComments} from '../../store.Comment'
-import {useSelector, useDispatch} from 'react-redux'
-import {NavLink} from 'react-router-dom'
-import '../Feed/post.css'
+import React from "react";
+import { getAllComments } from "../../store.Comment";
+import { useSelector, useDispatch } from "react-redux";
+import { NavLink } from "react-router-dom";
+import "../Feed/post.css";
+import CommentForm from '../../../api/forms/comment-form.py'
 
-
-const Comment = ({post_id}) => {
-    const comments = useSelector((state) => Object.values(state.comments));
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(getAllComments(post_id));
-      }, [dispatch]);
-    return(
-        <div className='commentContainer'>
-            {comments?.reverse().map(({user_id, content, username}) => (
-                <div className='commentBox'>
-                <div className='username_link'>
-                    <NavLink className='username_link' to={`/users/${user_id}`}>
-                        {username}
-                    </NavLink>
-                </div>
-                <div className='commentContent'>
-                    {content}
-                    </div>
-                </div>
-            ))}
+const Comment = ({ post_id }) => {
+  const sessionUser = useSelector((state) => state.session.user);
+  const comments = useSelector((state) => Object.values(state.comments));
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllComments(post_id));
+  }, [dispatch]);
+  return (
+    <div className="commentContainer">
+      {comments?.reverse().map(({ user_id, content, username }) => (
+        <div className="commentBox">
+          <div className="username_link">
+            <NavLink className="username_link" to={`/users/${user_id}`}>
+              {username}
+            </NavLink>
+          </div>
+          <div className="commentContent">{content}</div>
         </div>
-    )
-}
+      ))}
+      <CommentForm post_id={post_id} user_id={sessionUser.id}/>
+
+    </div>
+  );
+};
 
 export default Comment;
