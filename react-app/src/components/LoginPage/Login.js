47d0f45689c428e9822a2918f0a92934043d038e
hauck29 <anthony.hauck@aol.com>
2021-12-08 19:42:02 -0800

    code housekeeping

--- react-app/src/components/LoginPage/Login.js
+++ react-app/src/components/LoginPage/Login.js
@@ -1,23 +1,18 @@
 import LoginForm from "../auth/LoginForm";
 import { NavLink } from "react-router-dom";
-import Footer from "../Footer";
+import Footer from "../../components/Footer/Footer";
 import instagramme_logo from "./instagramme_logo_black.png";
-import app_store_logos from './app_store_logos.png'
-
 
 const LoginPage = () => {
   return (
     <>
-      {/* <h1>Signup Page</h1> */}
 
       <div className="fullContainer">
         <div className="mainContainer">
-          {/* <h1>Landing Page</h1> */}
           <div className="centerContainer">
             <div className="mainFormContainer">
               <div className="upperFormContainer">
                 <img className="formLogo" src={instagramme_logo}></img>
-
                 <LoginForm />
               </div>
               <div className="lowerFormContainer">
@@ -26,7 +21,7 @@ const LoginPage = () => {
                   Login
                 </NavLink>
               </div>
-              
+
             </div>
           </div>
         </div>
--- react-app/src/components/SignupPage/SignupPage.js
+++ react-app/src/components/SignupPage/SignupPage.js
@@ -1,13 +1,7 @@
 import SignUpForm from "../auth/SignUpForm";
 import { NavLink } from "react-router-dom";
-import Footer from "../Footer";
-// import "../LandingPage/LandingPage.css";
-// import './SignupPage.css'
-import phones from "./phones.png";
+import Footer from "../../components/Footer/Footer";
 import instagramme_logo from "./instagramme_logo_black.png";
-import profile_image from "./profileImage.jpg";
-import snow_pic from './snow.jpeg'
-import app_store_logos from './app_store_logos.png'
 import './SignupPage.css'
 
 const SignupPage = () => {
@@ -31,7 +25,7 @@ const SignupPage = () => {
                   Login
                 </NavLink>
               </div>
-              
+
             </div>
           </div>
         </div>
