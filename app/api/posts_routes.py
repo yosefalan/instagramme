3c0755690e2e3962060cb1849101aa5ed568276d
hauck29 <anthony.hauck@aol.com>
2021-12-09 13:07:50 -0800

    commented out comments routes

--- app/api/posts_routes.py
+++ app/api/posts_routes.py
@@ -99,74 +99,74 @@ def delete_post(id):
         return "Post not found", 404
 
 
-# READ ALL COMMENTS ON ONE POST
-@posts_routes.route('/<int:id>/comments')
-@login_required
-def get_comments(id):
-    post = Post.query.get(id)
-    if post:
-        comments = Comment.query.filter(
-            Comment.post_id == id).order_by(Comment.id.desc()).all()
-        comments_dict = {comment.id: comment.to_dict() for comment in comments}
+# # READ ALL COMMENTS ON ONE POST
+# @posts_routes.route('/<int:id>/comments')
+# @login_required
+# def get_comments(id):
+#     post = Post.query.get(id)
+#     if post:
+#         comments = Comment.query.filter(
+#             Comment.post_id == id).order_by(Comment.id.desc()).all()
+#         comments_dict = {comment.id: comment.to_dict() for comment in comments}
 
-        return comments_dict
+#         return comments_dict
 
-# CREATE NEW COMMENT ON ONE POST
-@posts_routes.route('/<int:id>/comments', methods=["POST"])
-@login_required
-def create_comment(id):
-    form = CommentForm()
+# # CREATE NEW COMMENT ON ONE POST
+# @posts_routes.route('/<int:id>/comments', methods=["POST"])
+# @login_required
+# def create_comment(id):
+#     form = CommentForm()
 
-    form['csrf_token'].data = request.cookies['csrf_token']
-    if form.validate_on_submit():
-        comment = Comment(
-            user_id=form.data['user_id'],
-            content=form.data['content'],
-            post_id=form.data['post_id'],
-            createdAt=datetime.datetime.now,
-            updatedAt=datetime.datetime.now
-        )
-        db.session.add(comment)
-        db.session.commit()
+#     form['csrf_token'].data = request.cookies['csrf_token']
+#     if form.validate_on_submit():
+#         comment = Comment(
+#             user_id=form.data['user_id'],
+#             content=form.data['content'],
+#             post_id=form.data['post_id'],
+#             createdAt=datetime.datetime.now,
+#             updatedAt=datetime.datetime.now
+#         )
+#         db.session.add(comment)
+#         db.session.commit()
 
-        return comment.to_dict()
-    return {'errors': validation_errors_to_error_messages(form.errors)}, 400
+#         return comment.to_dict()
+#     return {'errors': validation_errors_to_error_messages(form.errors)}, 400
 
-# UPDATE ONE COMMENT
-@posts_routes.route("/<int:id>/comments/<int:comment_id>", methods=["PUT"])
-@login_required
-def update_post(id, comment_id):
+# # UPDATE ONE COMMENT
+# @posts_routes.route("/<int:id>/comments/<int:comment_id>", methods=["PUT"])
+# @login_required
+# def update_post(id, comment_id):
 
-    ################### Is there a need to query for Post before deleting a comment?
+#     ################### Is there a need to query for Post before deleting a comment?
 
-    # post = Post.query.get(id)
-    # if post:
-    #     for key, value in request.form:
-    #         setattr(post, key, value)
-    #     db.session.commit()
-    #     return post.to_dict()
-    comment = Comment.query.get(comment_id)
-    if comment:
-        for key, value in request.form:
-            setattr(comment, key, value)
-        db.session.commit()
-        return comment.to_dict()
-    else:
-        return "Comment not found", 404
+#     # post = Post.query.get(id)
+#     # if post:
+#     #     for key, value in request.form:
+#     #         setattr(post, key, value)
+#     #     db.session.commit()
+#     #     return post.to_dict()
+#     comment = Comment.query.get(comment_id)
+#     if comment:
+#         for key, value in request.form:
+#             setattr(comment, key, value)
+#         db.session.commit()
+#         return comment.to_dict()
+#     else:
+#         return "Comment not found", 404
 
-# DELETE ONE COMMENT ON ONE POST
-@posts_routes.route("/<int:id>/comments/<int:comment_id>", methods=["DELETE"])
-@login_required
-def delete_comment(id, comment_id):
-    # post = Post.query.get(id)
-    # if post:
-    #     db.session.delete(post)
-    #     db.session.commit()
-    #     return "Ok", 200
-    comment = Comment.query.get(comment_id)
-    if comment:
-        db.session.delete(comment)
-        db.session.commit()
-        return 'Ok', 200
-    else:
-        return "Post not found", 404
+# # DELETE ONE COMMENT ON ONE POST
+# @posts_routes.route("/<int:id>/comments/<int:comment_id>", methods=["DELETE"])
+# @login_required
+# def delete_comment(id, comment_id):
+#     # post = Post.query.get(id)
+#     # if post:
+#     #     db.session.delete(post)
+#     #     db.session.commit()
+#     #     return "Ok", 200
+#     comment = Comment.query.get(comment_id)
+#     if comment:
+#         db.session.delete(comment)
+#         db.session.commit()
+#         return 'Ok', 200
+#     else:
+#         return "Post not found", 404
